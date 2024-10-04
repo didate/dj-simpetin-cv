c3afb6034d300c0436861c21106f22381a6ddedc
Diallo Mamadou Lamarana <lyve.diallo@gmail.com>
2024-10-04 10:29:10 -0400

    registering models in admin

--- resume/admin.py
+++ resume/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+# admin.py
+
+from django.contrib import admin
+from .models import Resume, WorkExperience, Education, Skill, Project, Certification, AdditionalInformation, Reference
+
+admin.site.register(Resume)
+admin.site.register(WorkExperience)
+admin.site.register(Education)
+admin.site.register(Skill)
+admin.site.register(Project)
+admin.site.register(Certification)
+admin.site.register(AdditionalInformation)
+admin.site.register(Reference)
